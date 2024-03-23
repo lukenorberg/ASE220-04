9a6d2759eaad86a728ab184702afec6aee0db4f2
luke <luke@deb.res6.spectrum.com>
2024-03-23 01:13:35 -0400

    fixed login issue

--- src/functions.js
+++ src/functions.js
@@ -1,38 +1,37 @@
-module.exports={
+module.exports = {
   // SIGN-UP VALIDATION
-	 validateEmail:function(email){
-	  return String(email)
-		.toLowerCase()
-		.match(
-		  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-		);
-	},
+  validateEmail: function(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  },
 
-	 validatePassword:function(password) {
-	  return (
-		/[A-Z]/.test(password) &&
-		/[a-z]/.test(password) &&
-		/[0-9]/.test(password) &&
-		/[^A-Za-z0-9]/.test(password) &&
-		password.length >= 8
-	  );
-	},
+  validatePassword: function(password) {
+    return (
+      /[A-Z]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /[0-9]/.test(password) &&
+      /[^A-Za-z0-9]/.test(password) &&
+      password.length >= 8
+    );
+  },
 
   // SIGN-IN VALIDATION
 
-	loginUser:function(userInfo) {
-	  const lineByLine = require("n-readlines");
-	  const CRED_FILE = "./data/credentials.csv";
-	  const liner = new lineByLine(CRED_FILE);
-	  let line;
+  loginUser: function(userInfo) {
+    const lineByLine = require("n-readlines");
+    const CRED_FILE = "./data/credentials.csv";
+    const liner = new lineByLine(CRED_FILE);
+    let line;
 
-	  while ((line = liner.next())) {
-		lineInfo = line.toString("ascii").split(",");
-		if (userInfo.email === lineInfo[0] && userInfo.password === lineInfo[1]) {
-		  return true;
-		}
-	  }
-	  return false;
-	}
+    while ((line = liner.next())) {
+      lineInfo = line.toString("ascii").split(",");
+      if (userInfo.email === lineInfo[0] && userInfo.password === lineInfo[1]) {
+        return true;
+      }
+    }
+    return false;
+  },
 };
-
--- src/server.js
+++ src/server.js
@@ -1,47 +1,46 @@
-const http=require('node:http');
-const fs=require('node:fs');
+const http = require("node:http");
+const fs = require("node:fs");
 
-const fcn=require('./functions.js');
+const fcn = require("./functions.js");
 
-const server=http.createServer((request,response)=>{
+const server = http.createServer((request, response) => {
   console.log(request.url);
-  let htmlFile=fs.readFileSync('./index.html','utf8');
-  if(request.url.includes('/api/signin/')){
-	  let data=request.url.split('/');
-	  console.log(data);
-	  let userInfo={
-		email:data[3],
-		password:data[4]
-	  };
-	  let login=fcn.loginUser(userInfo);
-	  if(!login)
-	  {
-		response.write(`<h1>Incorrect credentials</h1>`);
-	  }else{
-		response.write(`<h1>Successfully signed in</h1>`);
-	  }
-  }else{
-	if(request.url.includes('/api/signup/')){
-	  
-	  let data=request.url.split('/');
-	  console.log(data);
-	  let email=fcn.validateEmail(data[3]);
-	  let pass=fcn.validatePassword(data[4]);
-	  if(!email || !pass)
-	  {
-		response.write(`<h1>Email or password is not in the correct format</h1>`);
-	  }else{
-		response.write(`<h1>Successfully created account</h1>`);
-	  }
-    }else{
-	  const error={
-		status:-1,
-		error:"unauthorized request"
-	  };
-	  response.writeHead(401, {
-        'Content-Type': 'application/json'
+  let htmlFile = fs.readFileSync("./index.html", "utf8");
+  if (request.url.includes("/api/signin/")) {
+    let data = request.url.split("/");
+    console.log(data);
+    let userInfo = {
+      email: data[3],
+      password: data[4],
+    };
+    let login = fcn.loginUser(userInfo);
+    if (!login) {
+      response.write(`<h1>Incorrect credentials</h1>`);
+    } else {
+      response.write(`<h1>Successfully signed in</h1>`);
+    }
+  } else {
+    if (request.url.includes("/api/signup/")) {
+      let data = request.url.split("/");
+      console.log(data);
+      let email = fcn.validateEmail(data[3]);
+      let pass = fcn.validatePassword(data[4]);
+      if (!email || !pass) {
+        response.write(
+          `<h1>Email or password is not in the correct format</h1>`,
+        );
+      } else {
+        response.write(`<h1>Successfully created account</h1>`);
+      }
+    } else {
+      const error = {
+        status: -1,
+        error: "unauthorized request",
+      };
+      response.writeHead(401, {
+        "Content-Type": "application/json",
       });
-	  response.write(JSON.stringify(error));
+      response.write(JSON.stringify(error));
     }
   }
   response.write(htmlFile);
